098d7b62aa0f80d74c70c6f2e8bacd2560adfd56
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2018-11-07 16:37:40 +0800

    Initial commit

---
+++ Bingo/Bingo.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Bingo
+{
+    partial class Bingo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bingo));
+            this.lblLetter = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblDrawn = new System.Windows.Forms.Label();
+            this.tmMain1 = new System.Windows.Forms.Timer(this.components);
+            this.tmCtrl1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtAll = new System.Windows.Forms.TextBox();
+            this.lblDrawsLeft = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.tmMain2 = new System.Windows.Forms.Timer(this.components);
+            this.tmCtrl2 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLetter
+            // 
+            this.lblLetter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetter.ForeColor = System.Drawing.Color.Black;
+            this.lblLetter.Location = new System.Drawing.Point(153, 62);
+            this.lblLetter.Name = "lblLetter";
+            this.lblLetter.Size = new System.Drawing.Size(240, 248);
+            this.lblLetter.TabIndex = 0;
+            this.lblLetter.Text = "--";
+            this.lblLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(775, 62);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(128, 75);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblDrawn
+            // 
+            this.lblDrawn.AutoSize = true;
+            this.lblDrawn.BackColor = System.Drawing.Color.Transparent;
+            this.lblDrawn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrawn.ForeColor = System.Drawing.Color.Black;
+            this.lblDrawn.Location = new System.Drawing.Point(6, 3);
+            this.lblDrawn.MaximumSize = new System.Drawing.Size(1000, 0);
+            this.lblDrawn.Name = "lblDrawn";
+            this.lblDrawn.Size = new System.Drawing.Size(0, 29);
+            this.lblDrawn.TabIndex = 2;
+            // 
+            // tmMain1
+            // 
+            this.tmMain1.Interval = 10;
+            this.tmMain1.Tick += new System.EventHandler(this.tmMain1_Tick);
+            // 
+            // tmCtrl1
+            // 
+            this.tmCtrl1.Interval = 500;
+            this.tmCtrl1.Tick += new System.EventHandler(this.tmCtrl1_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(775, 152);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(128, 75);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtAll
+            // 
+            this.txtAll.BackColor = System.Drawing.SystemColors.Window;
+            this.txtAll.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAll.Location = new System.Drawing.Point(0, 0);
+            this.txtAll.MaximumSize = new System.Drawing.Size(1000, 0);
+            this.txtAll.Multiline = true;
+            this.txtAll.Name = "txtAll";
+            this.txtAll.ReadOnly = true;
+            this.txtAll.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAll.Size = new System.Drawing.Size(1000, 0);
+            this.txtAll.TabIndex = 1;
+            // 
+            // lblDrawsLeft
+            // 
+            this.lblDrawsLeft.AutoSize = true;
+            this.lblDrawsLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDrawsLeft.Location = new System.Drawing.Point(413, 320);
+            this.lblDrawsLeft.Name = "lblDrawsLeft";
+            this.lblDrawsLeft.Size = new System.Drawing.Size(118, 22);
+            this.lblDrawsLeft.TabIndex = 6;
+            this.lblDrawsLeft.Text = "Draws Left:  75";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.lblNumber);
+            this.panel1.Controls.Add(this.lblDrawsLeft);
+            this.panel1.Controls.Add(this.lblLetter);
+            this.panel1.Controls.Add(this.btnGenerate);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 487);
+            this.panel1.TabIndex = 7;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.ForeColor = System.Drawing.Color.Black;
+            this.lblNumber.Location = new System.Drawing.Point(399, 62);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(334, 248);
+            this.lblNumber.TabIndex = 7;
+            this.lblNumber.Text = "--";
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmMain2
+            // 
+            this.tmMain2.Interval = 10;
+            this.tmMain2.Tick += new System.EventHandler(this.tmMain2_Tick);
+            // 
+            // tmCtrl2
+            // 
+            this.tmCtrl2.Interval = 500;
+            this.tmCtrl2.Tick += new System.EventHandler(this.tmCtrl2_Tick);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl.Location = new System.Drawing.Point(0, 484);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1094, 150);
+            this.tabControl.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblDrawn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1086, 117);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Drawn";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtAll);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1086, 117);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "All Draws";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Bingo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 634);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Bingo";
+            this.Text = "Bingo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Bingo_Load);
+            this.SizeChanged += new System.EventHandler(this.Bingo_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLetter;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblDrawn;
+        private System.Windows.Forms.Timer tmMain1;
+        private System.Windows.Forms.Timer tmCtrl1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblDrawsLeft;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtAll;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Timer tmMain2;
+        private System.Windows.Forms.Timer tmCtrl2;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Bingo/Bingo.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Bingo
+{
+    public partial class Bingo : Form
+    {
+        int[] arr = Enumerable.Range(1, 75).ToArray();
+        string[] bingo = new string[75];
+        List<string> drawsList = new List<string>();
+        private bool pause = false;
+
+        public Bingo()
+        {
+            InitializeComponent();            
+        }
+
+        private void Bingo_Load(object sender, System.EventArgs e)
+        {
+            for (int i = 0; i <= (arr.Length - 1); i++)
+            {
+                if (arr[i] > 0) { bingo[i] = "B" + arr[i]; }
+                if (arr[i] > 15) { bingo[i] = "I" + arr[i]; }
+                if (arr[i] > 30) { bingo[i] = "N" + arr[i]; }
+                if (arr[i] > 45) { bingo[i] = "G" + arr[i]; }
+                if (arr[i] > 60) { bingo[i] = "O" + arr[i]; }
+            }
+
+            FixControls();
+        }
+
+        private void FixControls()
+        {
+            panel1.Height = panel1.Parent.Height - tabControl.Height;
+            panel1.Width = panel1.Parent.Width;
+
+            lblLetter.Left = (lblLetter.Parent.Width - lblLetter.Width - lblNumber.Width) / 2;
+            lblLetter.Top = ((lblLetter.Parent.Height - lblLetter.Height) / 2) + 30;
+
+            lblNumber.Left = lblLetter.Left + lblLetter.Width + 10;
+            lblNumber.Top = lblLetter.Top;
+
+            lblDrawsLeft.Left = (lblDrawsLeft.Parent.Width - lblDrawsLeft.Width) / 2;
+            lblDrawsLeft.Top = lblLetter.Top + lblLetter.Height + 10;
+
+            btnGenerate.Left = lblNumber.Width + lblNumber.Left + 10;
+            btnGenerate.Top = lblLetter.Top;
+
+            btnClear.Left = lblNumber.Width + lblNumber.Left + 10;
+            btnClear.Top = btnGenerate.Top + btnGenerate.Height + 10;
+
+            txtAll.Width = tabControl.Width;
+            txtAll.Height = tabControl.Height;
+            lblDrawn.Width = tabControl.Width;
+            lblDrawn.Height = tabControl.Height;
+            lblDrawn.MaximumSize = new Size(tabControl.Width, tabControl.Height);
+            txtAll.MaximumSize = new Size(tabControl.Width, tabControl.Height);
+        }
+
+        private void Bingo_SizeChanged(object sender, EventArgs e)
+        {
+            FixControls();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (!pause)
+            {
+                tmMain1.Interval = 50;
+                tmMain1.Enabled = true;
+                tmMain2.Interval = 100;
+                tmMain2.Enabled = true;
+                pause = true;
+                btnGenerate.Text = "Stop";
+
+                if (drawsList.Count == 75)
+                {
+                    btnGenerate.Enabled = false;
+                    lblDrawsLeft.Text = "Draws Left: 0";
+                }
+                else if (drawsList.Count >= 65)
+                {
+                    lblDrawsLeft.ForeColor = Color.Red;
+                }
+
+                lblDrawsLeft.Text = "Draws Left: " + (74 - drawsList.Count);
+            }
+            else
+            {
+                tmCtrl1.Enabled = true;
+                btnGenerate.Enabled = false;
+                pause = false;
+            }
+        }
+
+        private void show()
+        {
+            if (Convert.ToInt32(lblNumber.Text) > 0) { lblLetter.Text = "B"; }
+            if (Convert.ToInt32(lblNumber.Text) > 15) { lblLetter.Text = "I"; }
+            if (Convert.ToInt32(lblNumber.Text) > 30) { lblLetter.Text = "N"; }
+            if (Convert.ToInt32(lblNumber.Text) > 45) { lblLetter.Text = "G"; }
+            if (Convert.ToInt32(lblNumber.Text) > 60) { lblLetter.Text = "O"; }
+
+            string drawn = lblDrawn.Text;
+            lblDrawn.Text = drawn + lblLetter.Text + lblNumber.Text + "  ";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            btnGenerate.Enabled = true;
+
+            string allText = txtAll.Text + " \r\n \r\n" + lblDrawn.Text;
+            txtAll.Text = allText.TrimStart();
+
+            lblLetter.Text = "--";
+            lblNumber.Text = "--";
+            lblDrawn.Text = "";
+            lblDrawsLeft.Text = "Draws Left: 75";
+            lblDrawsLeft.ForeColor = Color.Black;
+            lblLetter.ForeColor = Color.Black;
+            lblNumber.ForeColor = Color.Black;
+
+            drawsList = new List<string>();
+        }
+
+        private void tmMain1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string draw = bingo.ToArray()[rnd.Next(0, bingo.Length)];
+
+            while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+            {
+                draw = bingo.ToArray()[rnd.Next(0, bingo.Length)];
+            }
+
+            lblLetter.Text = draw[0].ToString();           
+            lblLetter.ForeColor = Color.Red;            
+            lblNumber.ForeColor = Color.Red;
+        }
+
+        private void tmCtrl1_Tick(object sender, EventArgs e)
+        {
+            tmMain1.Interval += 40;
+            if (tmMain1.Interval >= 400)
+            {
+                lblLetter.ForeColor = Color.Green;
+                tmMain1.Enabled = false;
+                tmCtrl1.Enabled = false;
+            }
+        }
+
+        private void tmMain2_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string draw = string.Empty;
+
+            switch (lblLetter.Text)
+            {
+                case "B":
+                    draw = bingo.ToArray()[rnd.Next(0, 14)];
+
+                    while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+                    {
+                        draw = bingo.ToArray()[rnd.Next(0, 14)];
+                    }
+                    break;
+                case "I":
+                    draw = bingo.ToArray()[rnd.Next(15, 29)];
+
+                    while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+                    {
+                        draw = bingo.ToArray()[rnd.Next(15, 29)];
+                    }
+                    break;
+                case "N":
+                    draw = bingo.ToArray()[rnd.Next(30, 44)];
+
+                    while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+                    {
+                        draw = bingo.ToArray()[rnd.Next(30, 44)];
+                    }
+                    break;
+                case "G":
+                    draw = bingo.ToArray()[rnd.Next(45, 59)];
+
+                    while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+                    {
+                        draw = bingo.ToArray()[rnd.Next(45, 59)];
+                    }
+                    break;
+                case "O":
+                    draw = bingo.ToArray()[rnd.Next(60, 74)];
+
+                    while (drawsList.Contains(draw, StringComparer.Ordinal) && drawsList.Count > 1)
+                    {
+                        draw = bingo.ToArray()[rnd.Next(60, 74)];
+                    }
+                    break;
+            }
+
+            lblNumber.Text = draw.Remove(0, 1);
+
+            if (tmMain1.Enabled == false)
+            {
+                tmMain2.Interval += 40;
+                if (tmMain2.Interval >= 400)
+                {
+                    tmCtrl2.Enabled = true;
+                }
+            }            
+        }
+
+        private void tmCtrl2_Tick(object sender, EventArgs e)
+        {
+            lblNumber.ForeColor = Color.Green;
+            tmMain2.Enabled = false;
+            tmCtrl2.Enabled = false;
+            btnGenerate.Text = "Generate";
+            btnGenerate.Enabled = true;
+
+            show();
+            drawsList.Add(lblLetter.Text + lblNumber.Text);
+        }
+    }
+}
